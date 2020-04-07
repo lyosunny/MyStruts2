ae91afe852ca9b07decdf5ca3c445f3f1cdae456
Derek LAM <Derek LAM@DESKTOP-BMD9PEF>
2020-04-07 19:21:31 +0800

    Use * to map action

---
+++ MyStruts2/src/main/java/com/dlcreation/struts2test/action/BookAction.java
@@ -0,0 +1,24 @@
+package com.dlcreation.struts2test.action;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+public class BookAction extends ActionSupport {
+
+	private static final long serialVersionUID = 5464162890741009113L;
+
+	public String addBook() {
+		return SUCCESS;
+	}
+
+	public String deleteBook() {
+		return SUCCESS;
+	}
+
+	public String updateBook() {
+		return SUCCESS;
+	}
+
+	public String findBook() {
+		return SUCCESS;
+	}
+}
